7f36548e25641d3efbe70e45d3890ed0f2259889
ABSanthosh <a.b.santhosh02@gmail.com>
2022-03-20 21:10:12 +0530

    new "/store" route to store responses

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 const router = require("express").Router();
 const { Deta } = require("deta");
 
+const deta = Deta("c0m7xlcx_xZx4dZnho4d7wtAUeDtjECxuMFYsbfFC");
+const db = deta.Base("MockEvent");
+
 router.get("/", (req, res) => {
   res.status(200).json({ user: "none" });
+});
+
+router.post("/store", async (req, res) => {
+  const dataResponse = req.body["Data"];
+
+  const Mail = dataResponse.Email;
+  const Responses = JSON.stringify(dataResponse.Responses);
+
+  const toCreate = { Mail, Responses };
+  const insertedUser = await db.put(toCreate);
+  res.status(201).json(insertedUser);
 });
 
 module.exports = router;
